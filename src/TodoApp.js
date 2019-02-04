f23a1fab2693f7d02081ba0b293dbf8cf8ccb6bc
Camilo Lopez <camilino123@gmail.com>
2019-02-04 11:49:55 -0500

    App.js refactored

---
+++ src/TodoApp.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import moment from "moment";
+import {TodoList} from "./TodoList";
+import {Login} from "./component/Login";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: [], text: '', priority: 0, dueDate: moment()};
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} className="todo-form">
+                    <h3>New TODO</h3>
+                    <label htmlFor="text" className="right-margin">
+                        Text:
+                    </label>
+
+                    <input
+                        id="text"
+                        onChange={this.handleTextChange}
+                        value={this.state.text}>
+                    </input>
+
+                    <br/>
+                    <br/>
+                    <label htmlFor="priority" className="right-margin">
+                        Priority:
+                    </label>
+
+                    <input
+                        id="priority"
+                        type="number"
+                        onChange={this.handlePriorityChange}
+                        value={this.state.priority}>
+                    </input>
+
+                    <br/>
+                    <br/>
+
+                    <DatePicker
+                        id="due-date"
+                        selected={this.state.dueDate}
+                        placeholderText="Due date"
+                        onChange={this.handleDateChange}>
+                    </DatePicker>
+
+                    <br/>
+
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
+                <TodoList todoList={this.state.items}/>
+                <Login/>
+            </div>
+        );
+    }
+
+    handleTextChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    handlePriorityChange(e) {
+        this.setState({
+            priority: e.target.value
+        });
+    }
+
+    handleDateChange(date) {
+        this.setState({
+            dueDate: date
+        });
+    }
+
+    handleSubmit(e) {
+
+        e.preventDefault();
+
+        if (!this.state.text.length || !this.state.priority.length || !this.state.dueDate)
+            return;
+
+        const newItem = {
+            text: this.state.text,
+            priority: this.state.priority,
+            dueDate: this.state.dueDate,
+
+        };
+        this.setState(prevState => ({
+            items: prevState.items.concat(newItem),
+            text: '',
+            priority: '',
+            dueDate: ''
+        }));
+    }
+}
